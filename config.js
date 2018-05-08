9c349bd47ded293dd0f45d76984dbc5dae9b3b75
Michal <michalplebanski@gmail.com>
2018-05-08 21:33:32 +0200

    first commit

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '882842329903-7fhcb0kq2dkhj1ufkr0s8v6679sv9jr6.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'zm4huo5nBpmR9sieL9ew83fO',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
